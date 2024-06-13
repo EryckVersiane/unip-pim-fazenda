c4974c5c8d4c85b16cb3a9fe8d17944a4e125b5b
Eryck Versiane Couto <eryckcouto2005@gmail.com>
2024-06-13 16:06:12 -0300

    novas implementacoes

---
+++ Models/Pessoa.cs
@@ -0,0 +1,7 @@
+namespace MeuAppRestful.Models
+{
+    public class Pessoa {
+        public int Id {get; set;}
+        public string nome {get; set;}
+    }
+}
---
+++ Models/PessoaFisica.cs
@@ -0,0 +1,10 @@
+namespace MeuAppRestful.Models 
+{
+    public class PessoaFisica: Pessoa {
+
+        public int Id {get; set;}
+        public string CPF {get; set;}
+        public List<Telefone> Telefones {get; set;}
+
+    }
+}
